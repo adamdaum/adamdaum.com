337f607385783dee0c9ee4dd32a3384d37011e10
Adam <daumadam@gmail.com>
2018-07-28 11:36:35 -0700

    changed to serve a basic bootstrap layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Adam Daum' });
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+router.get('/Home', function(req, res, next) {
+  //res.render('index', { title: 'Adam Daum' });
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+/* GET resume page. */
+router.get('/Resume', function(req, res, next) {
+  //res.render('index', { title: 'Adam Daum' });
+  res.sendFile(path.join(__dirname, '../views/resume.html'));
 });
 
 module.exports = router;
+
